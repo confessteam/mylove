6e7afc0cd568ca0d9d4aa65d5e38dbdf09e21ad1
fanfanstl <2094531487@qq.com>
2019-01-17 14:27:37 +0800

    first init

---
+++ src/pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="hello">{{msg}}</div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'home'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
